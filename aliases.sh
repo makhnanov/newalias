8ccab6d60ce395b3cf07ec56e2a72c7a2e1471aa
romaxa <bakirov.important@gmail.com>
2021-12-12 21:58:37 +0700

    Add rwx and t aliases. Add some todo

--- aliases.sh
+++ aliases.sh
@@ -1,16 +1,32 @@
+# ToDo: import as external file
+# ToDo: add description
+# ToDo: cp to readme
+# ToDo: self update
+# ToDo: add usefull prograams
+# git mc docker docker-compose translate-shell
+
+#
 #
 alias newalias='sudo nano /etc/bash.bashrc && . /etc/bash.bashrc'
 #
+#
 alias hosts='sudo nano /etc/hosts'
+#
 #
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 #
+#
 alias c='clear'
+#
 #
 alias nrest='sudo service nginx restart'
 alias nconf='sudo mc /etc/nginx'
 alias nerror='cat /var/log/nginx/error.log'
 #
-alias keepassx="sudo nohup keepassx > /dev/null 2> /dev/null &"
+#
+alias rwx="sudo chmod -R 777"
+#
+#
+alias t='trans -target ru+en'
